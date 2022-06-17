c0d6ef6e24dace8bf11d3cb7619382f4b13b716e
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-06-16 22:34:37 -0300

    feat: sections created

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,57 @@ const showMenu = (toggleId, navId) => {
 showMenu("nav-toggle", "nav-menu");
 
 // removing menu mobile
+const navLink = document.querySelectorAll(".nav__link");
 
-// when click on each nav__link, it removes the show-menu class
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // scroll sections active link
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
 
 // change background header
+function scrollHeader() {
+  const nav = document.getElementById("header");
 
-// when the scroll is greater than 200vh, add the scroll-header class to the header
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+// show scroll top
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+
+  if (this.scrollY >= 560) scrollTop.classList.add("scroll-top");
+  else scrollTop.classList.remove("scroll-top");
+}
+
+window.addEventListener("scroll", scrollTop);
+
+// dark/light mode
